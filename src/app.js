873bd765556269296bebff5b66753c1491640c1e
c-su <m.y.like.music.10@gmail.com>
2019-10-08 19:46:35 +0900

    feat: cors対応

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const route = require('./route/router');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Controll-Allow-Origin', '*');
+  next();
+});
+
 app.use(route);
 
 module.exports = app;
